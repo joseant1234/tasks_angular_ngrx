1049e967aa426ff8f6a1b228cc1649cb4c7fbd7d
Jose Tafur <j-osea-nt@hotmail.com>
2020-06-27 18:48:12 -0500

    showing pendings task

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import * as actions from '../../filter/filter.actions';
 
 @Component({
   selector: 'app-todo-footer',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFooterComponent implements OnInit {
 
-  constructor() { }
+  currentFilter: actions.validFilters = 'all';
+  filters: actions.validFilters[] = ['all', 'completed', 'pendings'];
+  pendings = 0;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    // this.store.select('filter').subscribe(filter => {
+    //   this.currentFilter = filter;
+    // });
+    this.store.subscribe(state => {
+      this.currentFilter = state.filter;
+      this.pendings = state.todos.filter(todo => !todo.completed).length;
+    });
+  }
+
+  selectFilter(filter: actions.validFilters): void {
+    this.store.dispatch(actions.setFilter({ filter }));
   }
 
 }
